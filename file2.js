3f792b5c4d1e602f3d41a264293141e60dc59721
rhzzcjj <civilian_x@163.com>
2019-08-12 17:58:16 +0800

    update

--- file2.js
+++ file2.js
@@ -1,17 +1,47 @@
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-        var ctx = canvas.getContext('2d');
-
-        ctx.beginPath();
-        ctx.arc(75,75,50,0,Math.PI*2,true); // 绘制
-        ctx.moveTo(110,75);
-        ctx.arc(75,75,35,0,Math.PI,false);   // 口(顺时针)
-        ctx.moveTo(65,65);
-        ctx.arc(60,65,5,0,Math.PI*2,true);  // 左眼
-        ctx.moveTo(95,65);
-        ctx.arc(90,65,5,0,Math.PI*2,true);  // 右眼
-        ctx.stroke();
-    }
+var sun = new Image();
+var moon = new Image();
+var earth = new Image();
+function init(){
+    sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+    moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+    earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
+    window.requestAnimationFrame(draw);
 }
-draw();
+
+function draw() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0,0,300,300); // clear canvas
+
+    ctx.fillStyle = 'rgba(0,0,0,0.4)';
+    ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+    ctx.save();
+    ctx.translate(150,150);
+
+    // Earth
+    var time = new Date();
+    ctx.rotate( ((2*Math.PI)/60)*time.getSeconds() + ((2*Math.PI)/60000)*time.getMilliseconds() );
+    ctx.translate(105,0);
+    ctx.fillRect(0,-12,40,24); // Shadow
+    ctx.drawImage(earth,-12,-12);
+
+    // Moon
+    ctx.save();
+    ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+    ctx.translate(0,28.5);
+    ctx.drawImage(moon,-3.5,-3.5);
+    ctx.restore();
+
+    ctx.restore();
+
+    ctx.beginPath();
+    ctx.arc(150,150,105,0,Math.PI*2,false); // Earth orbit
+    ctx.stroke();
+
+    ctx.drawImage(sun,0,0,300,300);
+
+    window.requestAnimationFrame(draw);
+}
+
+init();
