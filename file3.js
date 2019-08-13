7e9ba8772ac42ec77ac70dafe593462cb93079a5
rhzzcjj <civilian_x@163.com>
2019-08-13 18:10:28 +0800

    update

--- file3.js
+++ file3.js
@@ -1,73 +1,142 @@
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var raf;
 var running = false;
 
-var ball = {
-    
-    x: 100,
-    y: 100,
-    vx: 5,
-    vy: 1,
-    radius: 25,
-    color: 'red',
-    draw: function() {
+function RectImage () {
+
+    var that = this;
+    this.x = 200;
+    this.y = 100;
+    this.width = 75;
+    this.height = 95;
+    this.vx = 5;
+    this.vy = 1;
+    this.color = 'red';
+
+    this.centerX = this.x + this.width / 2;
+    this.centerY = this.y + this.height + 25;
+    this.radius = 10;
+
+    this.isMouseDown = false;
+    this.isMouseUp = false;
+    this.isRotating = false;
+    this.isSelected = false;
+
+    this.drawImage = function() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.rect(that.x, that.y, that.width, that.height);
         ctx.closePath();
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    },
-    rotateButton: {
-        draw: function() {
-            ctx.beginPath();
-            ctx.arc
-        }
-    },
-};
+        ctx.save();
+        ctx.strokeStyle = that.color;
+        ctx.stroke();
+        ctx.restore();
+    };
+    this.drawButton = function() {
+        ctx.beginPath();
+        ctx.arc(that.centerX, that.centerY, that.radius, 0, 2 * Math.PI, true);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(that.centerX, that.centerY, that.radius / 2, 0, 2 * Math.PI, true);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.moveTo(that.x + that.width / 2, that.y + that.height);
+        ctx.lineTo(that.centerX,that.centerY - that.radius);
+        ctx.stroke();
+        ctx.closePath();
+    };
+    this.inRect = function (offsetX, offsetY) {
+        return offsetX >= that.x && offsetX <= that.x + that.width
+            && offsetY >= that.y && offsetY <= that.y + that.height;
+    };
+    this.inCircular = function (offsetX, offsetY) {
+        return Math.sqrt(Math.pow(offsetX - that.centerX, 2) + Math.pow(offsetY - that.centerY, 2)) <= that.radius;
+    }
+}
 
 
-
-function clear() {
-    ctx.fillStyle = 'rgba(255,255,255,0.3)';
+function clear(transparency = 1) {
+    ctx.fillStyle = 'rgba(255, 255, 255, ' + transparency + ')';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 }
 
 function draw() {
     clear();
-    ball.draw();
-    ball.x += ball.vx;
-    ball.y += ball.vy;
+    var image = new RectImage();
+    image.drawImage();
+    image.x += image.vx;
+    image.y += image.vy;
 
-    if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
-        ball.vy = -ball.vy;
+    if (image.y + image.vy > canvas.height || image.y + image.vy < 0) {
+        image.vy = -image.vy;
     }
-    if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
-        ball.vx = -ball.vx;
+    if (image.x + image.vx > canvas.width || image.x + image.vx < 0) {
+        image.vx = -image.vx;
     }
 
     raf = window.requestAnimationFrame(draw);
 }
 
-canvas.addEventListener('mousemove', function(e){
-    if (!running) {
-        clear();
-        ball.x = e.clientX;
-        ball.y = e.clientY;
-        ball.draw();
-    }
-});
+function main() {
+    var image = new RectImage();
 
-canvas.addEventListener('click',function(e){
-    if (!running) {
-        raf = window.requestAnimationFrame(draw);
-        running = true;
-    }
-});
+    canvas.addEventListener('click',function(e){
+        // if (!running) {
+        //     raf = window.requestAnimationFrame(draw);
+        //     running = true;
+        // }
+        if (image.inRect(e.offsetX, e.offsetY) && !image.isSelected) {
+            image.isSelected = true;
+            clear();
+            image.drawImage();
+            image.drawButton();
+        } else if (image.inRect(e.offsetX, e.offsetY) && image.isSelected) {
+            image.isSelected = false;
+            clear();
+            image.drawImage();
+        }
+    });
 
-canvas.addEventListener('mouseout', function(e){
-    window.cancelAnimationFrame(raf);
-    running = false;
-});
+    canvas.addEventListener('mousedown', function(e){
+        image.isMouseDown = true;
+        if (image.inCircular(e.offsetX, e.offsetY)) {
+            image.isRotating = true;
+            clear(0.3);
+        }
+    });
 
-ball.draw();
+    canvas.addEventListener('mouseup', function(e){
+        image.isMouseDown = false;
+        if (image.isRotating) {
+            image.isRotating = false;
+            clear();
+            image.drawImage();
+            image.drawButton();
+        }
+    });
+
+    canvas.addEventListener('mousemove', function(e){
+        // if (!running) {
+        //     clear();
+        //     image.x = e.offsetX;
+        //     image.y = e.offsetY;
+        //     image.draw();
+        // }
+        if (image.isMouseDown) {
+
+        }
+    });
+
+    canvas.addEventListener('mouseout', function(e){
+        window.cancelAnimationFrame(raf);
+        running = false;
+    });
+
+    image.drawImage();
+}
+
+main();
